2f0d33d46cdae4853fe788936751f85360becd02
myagmarjavdulguun <90972952+myagmarjavdulguun@users.noreply.github.com>
2023-10-27 17:26:27 +0800

    Add files via upload

---
+++ home.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Cookies from 'js-cookie';
+
+function Home() {
+    const router = useRouter();
+    const [name, setName] = useState('');
+    var loggedUser;
+    useEffect(() => {
+        loggedUser = Cookies.get('loggedUser');
+        if (!loggedUser) {
+            router.push('/login');
+        } else {
+            setName(loggedUser.charAt(0).toUpperCase() + loggedUser.slice(1).toLowerCase());
+        }
+    }, [router]);
+
+    const handleLogout = () => {
+        Cookies.remove('loggedUser');
+        router.push('/login');
+    };
+
+    return (
+        <div>
+            {loggedUser ? <div>
+                <h1>Home</h1>
+                {<p>Welcome to the home page, {name}!</p>}
+                <button onClick={handleLogout}>Logout</button>
+            </div> : null}
+        <style jsx global>{`
+            html, body {
+                padding: 0;
+                margin: 0;
+                font-family: Segoe UI;
+                display: flex;
+                justify-content: center;
+                font-size: 30px;
+            }
+            button {
+            height: 30px;
+            font-size: 20px;
+            margin: 10px 10px;
+            width: 130px;
+            }
+        `}</style>
+        </div>
+    );
+}
+
+export default Home;
---
+++ login.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Cookies from 'js-cookie';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    try {
+        const loggedUser = Cookies.get('loggedUser');
+        if (loggedUser) {
+            router.push('/home');
+        }
+    } catch (error) {
+        console.error('Error checking login status:', error);
+    }
+  }, [router]);
+
+  function handleLogin(e) {
+    e.preventDefault();
+    try {
+        const users = JSON.parse(Cookies.get('users'));
+        if (users) {
+            for (let i = 0; i < users.length; i++) {
+                const user = users[i];
+                if (user.email === email && user.password === password) {
+                    Cookies.set('loggedUser', user.name);
+                    router.push('/home');
+                    return;
+                }
+            }
+        }
+        setError('Invalid email or password.');
+    } catch (error) {
+        console.error('Error parsing user cookie:', error);
+        setError('An error occurred. Please try again.');
+    }
+  }
+
+  function handleSignUp() {
+    router.push('/register');
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      {(
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          /><br></br>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          /><br></br>
+          <button type="submit">Login</button>
+          <button type="button" onClick={handleSignUp}>
+            Sign Up
+          </button>
+          {error && <p>{error}</p>}
+        </form>
+      )}
+      <style jsx global>{`
+        html, body {
+          padding: 0;
+          margin: 0;
+          font-family: Segoe UI;
+          display: flex;
+          justify-content: center;
+        }
+        input, button {
+          margin: 10px 0px;
+          width: 300px;
+          height: 30px;
+          font-size: 20px;
+        }
+        button {
+          margin: 10px 10px;
+          width: 130px;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default Login;
